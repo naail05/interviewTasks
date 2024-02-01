06244e141c737364a622a5e728ee980826f9cb2e
nomuun <nomuun_m@yahoo.com>
2024-02-01 16:42:43 -0500

    add method

--- nomuunPackage/week1/Finra.java
+++ nomuunPackage/week1/Finra.java
@@ -1,4 +1,17 @@
 package nomuunPackage.week1;
 
 public class Finra {
+    public static void finra(int number){
+        for (int i = 1; i < number; i++) {
+            if (i % 3 == 0){
+                System.out.println("FIN");
+            } else if (i % 5 == 0) {
+                System.out.println("RA");
+            } else if (i % 15 == 0) {
+                System.out.println("FINRA");
+            }else {
+                System.out.println(i+", ");
+            }
+        }
+    }
 }
