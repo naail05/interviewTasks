7ec70ca7dc6b07f4ef0b4331e6a96f7a2941b3c3
nomuun <nomuun_m@yahoo.com>
2024-02-01 16:39:51 -0500

    add method

---
+++ nomuunPackage/week1/OddOrEven.java
@@ -0,0 +1,11 @@
+package nomuunPackage.week1;
+
+public class OddOrEven {
+    public static void oddOrEven (int number){
+        if (number % 2 == 0){
+            System.out.println("Even");
+        }else {
+            System.out.println("Odd");
+        }
+    }
+}
