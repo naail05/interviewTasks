36183f66ee28b2cefe76c60b5c4481997798ef77
sveta <denisenkosveta@gmail.com>
2024-02-17 15:42:06 -0500

    Added three weeks.

--- src/Svetlana_solutions/week1/DevidingwithoutDevideOperator.java
+++ src/Svetlana_solutions/week1/DevidingwithoutDevideOperator.java
@@ -1,3 +1,5 @@
+package week1;
+
 public class DevidingwithoutDevideOperator {
 
     public static void main(String[] args) {
--- src/Svetlana_solutions/week1/FINRA.java
+++ src/Svetlana_solutions/week1/FINRA.java
@@ -1,3 +1,5 @@
+package week1;
+
 public class FINRA {
 
     public static void main(String[] args) {
